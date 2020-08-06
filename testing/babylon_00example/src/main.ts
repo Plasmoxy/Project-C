f415b8ccae420fa0df631fec65da09ae7495273e
Plasmoxy <sebohacker@gmail.com>
2020-08-06 14:14:18 +0200

    hm

--- testing/babylon_00example/src/main.ts
+++ testing/babylon_00example/src/main.ts
@@ -1,11 +1,17 @@
 import * as BABYLON from 'babylonjs'
 import * as OIMO from 'oimo'
-import { Vector3, CannonJSPlugin, OimoJSPlugin, PhysicsImpostor } from 'babylonjs'
+import { Vector3, CannonJSPlugin, OimoJSPlugin, PhysicsImpostor, PhysicsViewer } from 'babylonjs'
 
 // extend window type for adding debug stuff
 declare global {
   interface Window {
     [x: string]: any
+  }
+  
+  interface Document {
+    mozPointerLockElement?: HTMLElement
+    webkitPointerLockElement?: HTMLElement
+    msPointerLockElement?: HTMLElement
   }
 }
 
@@ -15,6 +21,8 @@ class Game {
   private scene!: BABYLON.Scene;
   private camera!: BABYLON.FreeCamera;
   private light!: BABYLON.Light;
+  
+  isPointerLocked = false
 
   constructor(canvasElement : string) {
       // Create canvas and engine.
@@ -23,13 +31,19 @@ class Game {
   }
 
   createScene() : void {
+    
+      // ============ SCENE SETUP ============
+    
       // Create a basic BJS Scene object.
       this.scene = new BABYLON.Scene(this.engine);
+      
       this.scene.debugLayer.show()
       this.scene.collisionsEnabled = false
       this.scene.enablePhysics(null, new OimoJSPlugin(undefined, OIMO))
       this.scene.gravity = new Vector3(0, -0.1, 0)
-
+      
+      // ============= CAMERA ================
+      
       // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
       this.camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3(0, 3, -3), this.scene)
       this.camera.applyGravity = false
@@ -42,7 +56,51 @@ class Game {
       // Attach the camera to the canvas.
       this.camera.inputs.clear()
       this.camera.inputs.addMouse()
-      this.camera.attachControl(this.canvas, false);
+      this.camera.inertia = 0
+      this.camera.angularSensibility = 500
+      
+      // ============= POINTER LOCKING ====================
+      
+      const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement
+      this.scene.onPointerDown = (e) => {
+          if (document.pointerLockElement !== canvas) {
+            console.log('Was Already locked: ', document.pointerLockElement === canvas);
+
+            if (!this.isPointerLocked) {
+                canvas.requestPointerLock = canvas.requestPointerLock || canvas.msRequestPointerLock || canvas.mozRequestPointerLock || canvas.webkitRequestPointerLock || false;
+                if (canvas.requestPointerLock) {
+                    canvas.requestPointerLock();
+                }
+            }
+        }
+
+        //continue with shooting requests or whatever :P
+        //evt === 0 (left mouse click)
+        //evt === 1 (mouse wheel click (not scrolling))
+        //evt === 2 (right mouse click)
+      }
+      
+      // Event listener when the pointerlock is updated (or removed by pressing ESC for example).
+      const pointerlockchange = () => {
+        var controlEnabled = document.pointerLockElement || document.mozPointerLockElement || document.webkitPointerLockElement || document.msPointerLockElement || false;
+
+        // If the user is already locked
+        if (!controlEnabled) {
+            this.camera.detachControl(canvas);
+            this.isPointerLocked = false;
+        } else {
+            this.camera.attachControl(canvas);
+            this.isPointerLocked = true
+        }
+      };
+
+      // Attach events to the document
+      document.addEventListener("pointerlockchange", pointerlockchange, false);
+      document.addEventListener("mspointerlockchange", pointerlockchange, false);
+      document.addEventListener("mozpointerlockchange", pointerlockchange, false);
+      document.addEventListener("webkitpointerlockchange", pointerlockchange, false);
+
+      
 
       // Create a basic light, aiming 0,1,0 - meaning, to the sky.
       this.light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this.scene);
@@ -50,6 +108,7 @@ class Game {
       // Create a built-in "sphere" shape; with 16 segments and diameter of 2.
       let sphere = BABYLON.MeshBuilder.CreateSphere('sphere1',
                               {segments: 16, diameter: 2}, this.scene);
+      window.sphere = sphere
 
       // Move the sphere upward 1/2 of its height.
       sphere.position.y = 5;
@@ -61,9 +120,8 @@ class Game {
                               {width: 6, height: 6, subdivisions: 2}, this.scene);
       ground.physicsImpostor = new PhysicsImpostor(ground, PhysicsImpostor.PlaneImpostor, {mass: 0}, this.scene)
       
-      
-      
-      window.sphere = sphere
+      const physicsViewer = new PhysicsViewer(this.scene)
+      physicsViewer.showImpostor(sphere.physicsImpostor)
   }
 
   doRender() : void {
