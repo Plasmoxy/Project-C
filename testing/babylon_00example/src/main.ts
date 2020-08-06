01d0c0a302ddab161c8999d8e7fe43c23c80e997
Plasmoxy <sebohacker@gmail.com>
2020-08-06 13:39:01 +0200

    some cool physics works

--- testing/babylon_00example/src/main.ts
+++ testing/babylon_00example/src/main.ts
@@ -1,5 +1,13 @@
 import * as BABYLON from 'babylonjs'
-import { Vector3 } from 'babylonjs'
+import * as OIMO from 'oimo'
+import { Vector3, CannonJSPlugin, OimoJSPlugin, PhysicsImpostor } from 'babylonjs'
+
+// extend window type for adding debug stuff
+declare global {
+  interface Window {
+    [x: string]: any
+  }
+}
 
 class Game {
   private canvas: HTMLCanvasElement;
@@ -17,24 +25,23 @@ class Game {
   createScene() : void {
       // Create a basic BJS Scene object.
       this.scene = new BABYLON.Scene(this.engine);
-      this.scene.collisionsEnabled = true
-      this.scene.enablePhysics(new BABYLON.Vector3(0, -0.00, 0))
+      this.scene.debugLayer.show()
+      this.scene.collisionsEnabled = false
+      this.scene.enablePhysics(null, new OimoJSPlugin(undefined, OIMO))
+      this.scene.gravity = new Vector3(0, -0.1, 0)
 
       // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-      this.camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3(0, 5, -10), this.scene)
+      this.camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3(0, 3, -3), this.scene)
+      this.camera.applyGravity = false
       this.camera.ellipsoid = new Vector3(1, 1, 1)
-      this.camera.applyGravity = true
-      this.camera.speed = 0.5
-      this.camera.keysDown = [83]
-      this.camera.keysUp = [87]
-      this.camera.keysLeft = [65]
-      this.camera.keysRight = [68]
-      this.camera.checkCollisions = true
+      this.camera.speed = 0.1
 
       // Target the camera to scene origin.
       this.camera.setTarget(BABYLON.Vector3.Zero());
 
       // Attach the camera to the canvas.
+      this.camera.inputs.clear()
+      this.camera.inputs.addMouse()
       this.camera.attachControl(this.canvas, false);
 
       // Create a basic light, aiming 0,1,0 - meaning, to the sky.
@@ -45,12 +52,18 @@ class Game {
                               {segments: 16, diameter: 2}, this.scene);
 
       // Move the sphere upward 1/2 of its height.
-      sphere.position.y = 1;
+      sphere.position.y = 5;
+      sphere.physicsImpostor = new PhysicsImpostor(sphere, PhysicsImpostor.SphereImpostor, {mass: 1}, this.scene)
+      this.camera.parent = sphere
 
       // Create a built-in "ground" shape.
       let ground = BABYLON.MeshBuilder.CreateGround('ground1',
                               {width: 6, height: 6, subdivisions: 2}, this.scene);
-      ground.checkCollisions = true
+      ground.physicsImpostor = new PhysicsImpostor(ground, PhysicsImpostor.PlaneImpostor, {mass: 0}, this.scene)
+      
+      
+      
+      window.sphere = sphere
   }
 
   doRender() : void {
